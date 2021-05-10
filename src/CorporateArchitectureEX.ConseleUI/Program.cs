1e75ead44aa4af37ab6b9ee9036b9fa0722eeaea
parvizrovshanaliyev <parvizra@code.edu.az>
2021-05-10 20:57:46 +0400

    project structure 1

---
+++ src/CorporateArchitectureEX.ConseleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CorporateArchitectureEX.ConseleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
