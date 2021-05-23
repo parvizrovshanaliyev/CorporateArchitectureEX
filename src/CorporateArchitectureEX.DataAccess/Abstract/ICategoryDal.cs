cba341654a92675e1ba731626180520398edacd4
parvizrovshanaliyev <parvizra@code.edu.az>
2021-05-23 22:16:48 +0400

    IEntityRepository

---
+++ src/CorporateArchitectureEX.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CorporateArchitectureEX.Entities.Concrete;
+
+namespace CorporateArchitectureEX.DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> GetAll();
+        void Add(Category data);
+        void Update(Category data);
+        void Delete(Category data);
+        List<Product> GetAllByCategory(int id);
+    }
+}
