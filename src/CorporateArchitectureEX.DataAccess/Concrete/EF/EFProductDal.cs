9fb3c223a990e851ec4bb0935bc53d073f7c23e7
parvizrovshanaliyev <parvizra@code.edu.az>
2021-05-23 22:59:59 +0400

    ProductDal IDisposable pattern implementation of c# metodlari db ya uygunlasdirdim

--- src/CorporateArchitectureEX.DataAccess/Concrete/EF/EFProductDal.cs
+++ src/CorporateArchitectureEX.DataAccess/Concrete/EF/EFProductDal.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CorporateArchitectureEX.DataAccess.Abstract;
 using CorporateArchitectureEX.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace CorporateArchitectureEX.DataAccess.Concrete.EF
 {
@@ -15,27 +16,53 @@ namespace CorporateArchitectureEX.DataAccess.Concrete.EF
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            // IDisposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() 
+                    :context.Set<Product>().Where(filter).ToList();
+            }
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            // IDisposable pattern implementation of c#
+            using NorthwindContext context = new NorthwindContext();
+            return context.Set<Product>().SingleOrDefault(filter);
         }
 
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            // IDisposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            // IDisposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var modifiedEntity = context.Entry(entity);
+                modifiedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            // IDisposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public List<Product> GetAllByCategory(int id)
