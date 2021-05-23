79290d71889d7f81a0c2e3222bcf1e86c032d797
parvizrovshanaliyev <parvizra@code.edu.az>
2021-05-23 22:45:40 +0400

    EntityFrameworkCore.SqlServer packege added NorthwindDbContext, connection string

---
+++ src/CorporateArchitectureEX.DataAccess/Concrete/EF/EFCustomerDal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CorporateArchitectureEX.DataAccess.Abstract;
+using CorporateArchitectureEX.Entities.Concrete;
+
+namespace CorporateArchitectureEX.DataAccess.Concrete.EF
+{
+    public class EFCustomerDal:ICustomerDal
+    {
+        #region Implementation of IEntityRepository<Customer>
+
+        public List<Customer> GetAll(Expression<Func<Customer, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(Expression<Func<Customer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAllByCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/CorporateArchitectureEX.DataAccess/Concrete/EF/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using CorporateArchitectureEX.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorporateArchitectureEX.DataAccess.Concrete.EF
+{
+    public class NorthwindContext:DbContext
+    {
+        #region Overrides of DbContext
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        #endregion
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
